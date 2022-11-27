4e3e61c26b8a82c3acfa825b2802c92d72230c44
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-11-27 08:29:16 +0100

    FIX: Try demo app

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {Button} from 'rn-module/dist';
+import {Button} from 'rn-module';
 import {IUser, TCheck} from 'rn-module/dist/typings';
 
 const App = () => {
